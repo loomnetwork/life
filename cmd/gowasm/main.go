892f5522a7aa34dad32b2afae83569aac563c972
Michael(Zhiyi Weng) <vyloy.w@gmail.com>
2018-11-30 08:55:54 +0800

    add func to run wasm file directly

--- cmd/gowasm/main.go
+++ cmd/gowasm/main.go
@@ -2,62 +2,18 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/perlin-network/life/exec"
 	"github.com/perlin-network/life/gowasm"
-	"io/ioutil"
-	"time"
+	"os"
 )
 
 func main() {
-	entryFunctionFlag := flag.String("entry", "app_main", "entry function id")
-	jitFlag := flag.Bool("jit", false, "enable jit")
+	entryFunctionFlag := flag.String("entry", "run", "entry function id")
 	flag.Parse()
 
-	// Read WebAssembly *.wasm file.
-	input, err := ioutil.ReadFile(flag.Arg(0))
+	ret, err := gowasm.RunWASMFile(flag.Arg(0), *entryFunctionFlag)
 	if err != nil {
 		panic(err)
 	}
 
-	// Instantiate a new WebAssembly VM with a few resolved imports.
-	vm, err := exec.NewVirtualMachine(input, exec.VMConfig{
-		EnableJIT:          *jitFlag,
-		DefaultMemoryPages: 128,
-		DefaultTableSize:   65536,
-	}, gowasm.NewResolver(), nil)
-
-	if err != nil {
-		panic(err)
-	}
-
-	// Get the function ID of the entry function to be executed.
-	entryID, ok := vm.GetFunctionExport(*entryFunctionFlag)
-	if !ok {
-		fmt.Printf("Entry function %s not found; starting from 0.\n", *entryFunctionFlag)
-		entryID = 0
-	}
-
-	start := time.Now()
-
-	// If any function prior to the entry function was declared to be
-	// called by the module, run it first.
-	if vm.Module.Base.Start != nil {
-		startID := int(vm.Module.Base.Start.Index)
-		_, err := vm.Run(startID)
-		if err != nil {
-			vm.PrintStackTrace()
-			panic(err)
-		}
-	}
-
-	// Run the WebAssembly module's entry function.
-	ret, err := vm.Run(entryID, 0, 0)
-	if err != nil {
-		vm.PrintStackTrace()
-		panic(err)
-	}
-	end := time.Now()
-
-	fmt.Printf("return value = %d, duration = %v\n", ret, end.Sub(start))
+	os.Exit(int(ret))
 }
---
+++ gowasm/exec.go
@@ -0,0 +1,57 @@
+package gowasm
+
+import (
+	"fmt"
+	"github.com/perlin-network/life/exec"
+	"io/ioutil"
+)
+
+func RunWASMFile(path string, entry string) (ret int64, err error) {
+	if len(entry) < 1 {
+		entry = "run"
+	}
+
+	// Read WebAssembly *.wasm file.
+	input, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	// Instantiate a new WebAssembly VM with a few resolved imports.
+	vm, err := exec.NewVirtualMachine(input, exec.VMConfig{
+		EnableJIT:          false,
+		DefaultMemoryPages: 128,
+		DefaultTableSize:   65536,
+	}, NewResolver(), nil)
+
+	if err != nil {
+		return
+	}
+
+	// Get the function ID of the entry function to be executed.
+	entryID, ok := vm.GetFunctionExport(entry)
+	if !ok {
+		fmt.Printf("Entry function %s not found; starting from 0.\n", entry)
+		entryID = 0
+	}
+
+	// If any function prior to the entry function was declared to be
+	// called by the module, run it first.
+	if vm.Module.Base.Start != nil {
+		startID := int(vm.Module.Base.Start.Index)
+		_, err = vm.Run(startID)
+		if err != nil {
+			vm.PrintStackTrace()
+			return
+		}
+	}
+
+	// Run the WebAssembly module's entry function.
+	ret, err = vm.Run(entryID, 0, 0)
+	if err != nil {
+		vm.PrintStackTrace()
+		return
+	}
+
+	return
+}
